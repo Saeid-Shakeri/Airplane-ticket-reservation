8a0594219fd534290e37cb9e67d8fd342b476255
Saeid Shakeri <139858324+Saeid-Shakeri@users.noreply.github.com>
2024-01-04 01:53:46 +0330

    Add files via upload

--- Airline/flight/urls.py
+++ Airline/flight/urls.py
@@ -8,9 +8,13 @@ from . import views
 urlpatterns = [
        path('',views.Index.as_view(), name='index_view'),
        path('flight/',views.FlightList.as_view(),name="flight_list"),
-       path("<int:pk>/", views.FlightDetail.as_view(), name="flight_detail"),
+       path("flight/<int:pk>/", views.FlightDetail.as_view(), name="flight_detail"),
        path('airlines/',views.airlines_view , name='airlines_view'),
        path('airline/<int:pk>/',views.AirlineDetail.as_view(),name="airline_detail"),
-       path('search/',views.Search.as_view(),name="search")
+       path('search/',views.Search.as_view(),name="search"),
+       path('<int:pk>/ticket/',views.Ticket_view.as_view(),name="ticket"),
+     
+
+
 
 ]
--- Airline/flight/views.py
+++ Airline/flight/views.py
@@ -1,26 +1,28 @@
 from django.shortcuts import render
-from django.utils import timezone
 from django.http import HttpResponse
 from .models import Flight, Airline
-from .forms import SearchFrom
+from .forms import SearchFrom,TicketForm
 from django.template import loader
 from django.views import View
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from customer.models import order,Ticket
+from django.utils import timezone
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
+import logging
+
+
 
 
 
 class Index(View):
 
     def get(self, request):
-    #  q =  Flight.objects.filter(date__gt=timezone.now())
       template = loader.get_template("index.html")
       return HttpResponse(template.render({}))
 
 
-
     def post(self, request):
-    
          pass
 
 
@@ -30,10 +32,45 @@ class FlightList(ListView):
     model = Flight
     template_name = "flight_list.html"
 
+    def get(self,request):
+        q =  Flight.objects.filter(date__gt=timezone.now())
+
+        template = loader.get_template("flight_list.html")
+   
+        return HttpResponse(template.render({'flight_list': q}))
+
+
 
 class FlightDetail(DetailView):
     model = Flight
     template_name = "flight_detail.html"
+
+    
+    def post(self, request, *args, **kwargs):
+        pass
+        
+        
+
+
+class Ticket_view(LoginRequiredMixin,View):
+
+    def get(self, request,pk):
+         
+         form = TicketForm()
+         tic = Ticket.objects.filter(fly=pk).exclude(status="paid")
+         return render(request, "ticket.html", {"form":form, "ticket_list":tic})
+
+
+    def post(self, request,*args, **kwargs):
+           tic = Ticket.objects.get(unique_id=request.POST['unique_id'])
+           if(order.create(self,customer=request.user,tic=tic)):
+               return HttpResponse("Added to your shopping cart")
+        
+           else : return HttpResponse("Something went wrong")
+           
+
+           
+
 
 
 class AirlineDetail(DetailView):
@@ -48,14 +85,18 @@ class Search(View):
          form = SearchFrom()
          return render(request, "search.html", {"form":form})
 
-  
     def post(self, request):
            try:       
-                flight = Flight.objects.get(way = request.POST["way"],date=request.POST["date"])
-                return render(request, "flight_detail.html", {"object":flight})
+                flight = Flight.objects.filter(way=request.POST["way"],date=request.POST["date"])
+                return render(request, "flight_list.html", {"flight_list":flight})
 
            except:
                 return HttpResponse("this flight dosent exist!")
+
+
+
+
+
 
 
 
